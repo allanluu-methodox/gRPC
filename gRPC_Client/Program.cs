493f560baf924ec87181b7c545bd0dfc0e7dc3a8
allanluu-methodox <allan.luu@methodox.io>
2025-04-26 17:55:40 -0400

    Completed gRPC Server and Client
    One shot gRPC implementation with simple addition

---
+++ gRPC_Client/Program.cs
@@ -0,0 +1,8 @@
+﻿using Grpc.Net.Client;
+using gRPC_Server;
+
+var channel = GrpcChannel.ForAddress("http://localhost:7000");
+var client = new gRPC_Server.Math.MathClient(channel);
+
+var reply = await client.AddAsync(new AddRequest { A = 10, B = 20 });
+Console.WriteLine($"Result from server: {reply.Result}");
---
+++ gRPC_Server/Services/MathService.cs
@@ -0,0 +1,26 @@
+﻿using Grpc.Core;
+using gRPC_Server; // The generated namespace
+
+namespace gRPC_Server.Services
+{
+    public class MathService : Math.MathBase // Inherit from the generated base class
+    {
+        private readonly ILogger<MathService> _logger;
+
+        public MathService(ILogger<MathService> logger)
+        {
+            _logger = logger;
+        }
+
+        // Override the Add method defined in the generated MathBase class
+        public override Task<AddReply> Add(AddRequest request, ServerCallContext context)
+        {
+            _logger.LogInformation("Adding {A} + {B}", request.A, request.B);
+
+            return Task.FromResult(new AddReply
+            {
+                Result = request.A + request.B
+            });
+        }
+    }
+}
