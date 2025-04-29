7e1c9345a293bf7b311df23a6f7159142c8695cc
Charles Zhang <szinubuntu@gmail.com>
2025-04-29 15:59:05 -0400

    Add `requestCount` to protocol

--- gRPC_Client/Program.cs
+++ gRPC_Client/Program.cs
@@ -1,8 +1,13 @@
 ﻿using Grpc.Net.Client;
 using gRPC_Server;
 
-var channel = GrpcChannel.ForAddress("http://localhost:7000");
-var client = new gRPC_Server.Math.MathClient(channel);
+GrpcChannel channel = GrpcChannel.ForAddress("http://localhost:7000");
+gRPC_Server.Math.MathClient client = new gRPC_Server.Math.MathClient(channel);
 
-var reply = await client.AddAsync(new AddRequest { A = 10, B = 20 });
-Console.WriteLine($"Result from server: {reply.Result}");
+// Request once
+AddReply reply = await client.AddAsync(new AddRequest { A = 10, B = 20 });
+Console.WriteLine($"Result from server: {reply.Result} ({reply.RequestCount})");
+
+// Request twice
+AddReply reply2 = await client.AddAsync(new AddRequest { A = 10, B = 20 });
+Console.WriteLine($"Result from server: {reply.Result} ({reply.RequestCount})");
--- gRPC_Server/Services/MathService.cs
+++ gRPC_Server/Services/MathService.cs
@@ -6,20 +6,23 @@ namespace gRPC_Server.Services
     public class MathService : Math.MathBase // Inherit from the generated base class
     {
         private readonly ILogger<MathService> _logger;
-
         public MathService(ILogger<MathService> logger)
         {
             _logger = logger;
         }
 
+        public uint RequestCount = 0;
+
         // Override the Add method defined in the generated MathBase class
         public override Task<AddReply> Add(AddRequest request, ServerCallContext context)
         {
             _logger.LogInformation("Adding {A} + {B}", request.A, request.B);
+            RequestCount++;
 
             return Task.FromResult(new AddReply
             {
-                Result = request.A + request.B
+                Result = request.A + request.B,
+                RequestCount = RequestCount
             });
         }
     }
